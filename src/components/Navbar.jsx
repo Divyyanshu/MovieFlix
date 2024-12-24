83b40c444e14519c91ac0c5b5de33f6bfa9c7417
Divyyanshu <divvyyanshuutailor@gmail.com>
2024-12-25 00:08:55 +0530

    add movie project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>
+            <header className="m-2 h-14 p-5 flex items-center justify-between rounded-md bg-indigo-500 drop-shadow-lg">
+                <h1>MoviesFlex🍿</h1>
+                <div>
+                    <input
+                        type="text"
+                        className="input text-white w-96 p-2 rounded-md drop-shadow-md outline-none"
+                        placeholder="Search movies"
+                        autoFocus
+                    />
+                </div>
+                <div>
+                    <p>0 Movies found</p>
+                </div>
+            </header>
+        </div>
+    )
+}
+
+export default Navbar
